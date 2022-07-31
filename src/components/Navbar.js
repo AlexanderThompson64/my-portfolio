71611ef0b98255ba4daff4a722ffc3c1eff509fa
Alexander Thompson <Alexander.thompson.124@gmail.com>
2022-07-31 15:55:00 +0100

    Added styling for Navbar and set media queries up for a range of devices.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,11 @@ const Navbar = () => {
     <div className="Navbar">
       <nav className="navbar navbar-dark fixed-top">
         <div className="container-fluid">
-          <a className="navbar-brand ms-4" style={{}} href="/">
+          <a
+            className="navbar-brand ms-4"
+            style={{ fontFamily: "Dancing" }}
+            href="/"
+          >
             A.Thompson
           </a>
           <button
@@ -20,8 +24,12 @@ const Navbar = () => {
             tabIndex="-1"
             id="offcanvasNavbar"
             aria-labelledby="offcanvasNavbarLabel"
+            style={{backgroundColor: "#0B192E"}}
           >
-            <div className="offcanvas-header">
+            <div
+              className="offcanvas-header"
+              style={{ backgroundColor: "#0b192e" }}
+            >
               <div className="container">
                 <div className="row justify-content-end">
                   <button
@@ -33,17 +41,25 @@ const Navbar = () => {
                 </div>
                 <div className="row text-center">
                   <div className="col">
-                    <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                    <h5
+                      className="offcanvas-title"
+                      style={{ color: "whitesmoke", fontFamily: "Dancing" }}
+                      id="offcanvasNavbarLabel"
+                    >
                       A.Thompson
                     </h5>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="offcanvas-body lead">
+
+            <div
+              className="offcanvas-body lead"
+              style={{ backgroundColor: "#0b192e", fontFamily: "anak" }}
+            >
               <ul className="navbar-nav justify-content-end text-center flex-grow-1">
                 <li className="nav-item">
-                  <a className="nav-link mb-2" aria-current="fasls" href="/">
+                  <a className="nav-link mb-2" aria-current="false" href="/">
                     Home
                   </a>
                 </li>
@@ -65,26 +81,30 @@ const Navbar = () => {
               </ul>
               <br />
               <div className="container text-center">
-                <div className="container-fluid" id="icons">
+                <div className="container-fluid" id="offcanvasIcons">
                   <a
                     href="https://github.com/AlexanderThompson64"
                     className="bi bi-github me-4"
                     id="footer-github"
+                    style={{ color: "whitesmoke" }}
                   ></a>
                   <a
                     href="https://www.linkedin.com/in/alexander-thompson-095a171b7/"
                     className="bi bi-linkedin me-4"
                     id="footer-linkedin"
+                    style={{ color: "whitesmoke" }}
                   ></a>
                   <a
                     href="https://www.instagram.com/lexilex64/"
                     className="bi bi-instagram me-4"
                     id="footer-instagram"
+                    style={{ color: "whitesmoke" }}
                   ></a>
                   <a
                     href="https://twitter.com/theATdeveloper"
                     className="bi bi-twitter"
                     id="footer-twitter"
+                    style={{ color: "whitesmoke" }}
                   ></a>
                 </div>
               </div>
