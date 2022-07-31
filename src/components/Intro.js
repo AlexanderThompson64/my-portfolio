4e4123b54fe93e5127d463f5368129e7f7cecf4f
Alexander Thompson <Alexander.thompson.124@gmail.com>
2022-07-31 23:37:52 +0100

    Customized intro component for mobile first and different viewports, also adjusted navbar component for different viewports

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,13 @@ const Intro = () => {
       <div className="container-fluid">
         <p
           className=""
-          style={{ color: "#64ffda", marginTop: "100px", fontFamily: "anak", fontSize:"22px" }}
+          style={{
+            color: "#64ffda",
+            marginTop: "100px",
+            fontFamily: "anak",
+            fontSize: "22px",
+          }}
+          id="introHello"
         >
           Hello, my name is
         </p>
@@ -23,12 +29,17 @@ const Intro = () => {
         <p
           className="p-1"
           style={{ color: "whitesmoke", fontFamily: "anak", fontSize: "18px" }}
+          id="introParagraph"
         >
           I am a software development student studying my final year at the
           University of Sunderland. I am currently focusing on building
           applications using Javascipt and React.
         </p>
-        <div className="btn-group" role="group" style={{display: "none"}}>
+        <div
+          className="btn-group p-1"
+          role="group"
+          style={{ display: "none", fontFamily: "anak" }}
+        >
           <a
             href="https://github.com/AlexanderThompson64"
             className="btn btn-primary me-3 rounded"
@@ -38,6 +49,14 @@ const Intro = () => {
             Github
           </a>
           <a
+            href="https://www.instagram.com/lexilex64/"
+            className="btn btn-primary me-3 rounded"
+            id="btn-github"
+          >
+            <i className="bi bi-instagram me-1"></i>
+            Instagram
+          </a>
+          <a
             href="https://www.linkedin.com/in/alexander-thompson-095a171b7/"
             className="btn btn-primary me-3 rounded"
             id="btn-linkedin"
@@ -45,8 +64,20 @@ const Intro = () => {
             <i className="bi bi-linkedin me-1"></i>
             Linkedin
           </a>
+          <a
+            href="https://twitter.com/TheATdeveloper"
+            className="btn btn-primary me-3 rounded"
+            id="btn-linkedin"
+          >
+            <i className="bi bi-twitter me-1"></i>
+            Twitter
+          </a>
         </div>
-        <div className="container text-center" id="iconsBody" style={{color: "whitesmoke", fontSize: "25px", display: "none"}}>
+        <div
+          className="container text-center"
+          id="iconsBody"
+          style={{ color: "whitesmoke", fontSize: "25px", display: "none" }}
+        >
           <i className="bi bi-github me-4" id="footer-github"></i>
           <i className="bi bi-linkedin me-4" id="footer-linkedin"></i>
           <i className="bi bi-instagram me-4" id="footer-instagram"></i>
