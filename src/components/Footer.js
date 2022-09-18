d0c24d82804c654b151c64b6ad25f394311bada6
Alexander Thompson <alexander.thompson.124@gmail.com>
2022-09-18 12:26:39 +0000

    Fixed issue with footer not responding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,30 +3,30 @@ const Footer = () => {
     <div className="Footer">
       <div className="container d-flex justify-content-center">
         <div className="footerIcons" id="offcanvasIcons" style={{fontSize: "30px"}}>
-          <i
+          <a
             href="https://github.com/AlexanderThompson64"
             className="bi bi-github me-4"
             id="footer-github"
             style={{ color: "whitesmoke" }}
-          ></i>
-          <i
+          ></a>
+          <a
             href="https://www.linkedin.com/in/alexander-thompson-095a171b7/"
             className="bi bi-linkedin me-4"
             id="footer-linkedin"
             style={{ color: "whitesmoke" }}
-          ></i> 
-          <i
+          ></a> 
+          <a
             href="https://www.instagram.com/lexilex64/"
             className="bi bi-instagram me-4"
             id="footer-instagram"
             style={{ color: "whitesmoke" }}
-          ></i>
-          <i
+          ></a>
+          <a
             href="https://twitter.com/theATdeveloper"
             className="bi bi-twitter"
             id="footer-twitter"
             style={{ color: "whitesmoke" }}
-          ></i>
+          ></a>
         </div>
       </div>
       <br />
